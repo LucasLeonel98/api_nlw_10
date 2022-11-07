2e7b525e184460778a7165eb3730c69ffd7224ca
Lucas Leonel <lucasap_98@hotmail.com>
2022-11-07 19:43:56 -0300

    tests host

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
     await fastify.register(userRoutes)
     await fastify.register(authRoutes)
 
-    await fastify.listen({ port: process.env.PORT || 3333, host: 'api-poll-world-cup.herokuapp.com' })
+    await fastify.listen({ port: process.env.PORT || 3333, host: '164.163.237.35' })
 
 }
 
